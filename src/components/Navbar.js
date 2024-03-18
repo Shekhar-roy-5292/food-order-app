bd54b19349e236a99416450c603504f6664c9e96
shekhar-roy <shekharroy5292@gmail.com>
2024-03-18 12:17:40 +0530

    add react-router-dom and  material ui, icon library

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='leftSide'>left side</div>
+        <div className='rightSide'>right side</div>
+    </div>
+  );
+}
+
+export default Navbar
