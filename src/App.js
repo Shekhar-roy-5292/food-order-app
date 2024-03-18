15c5587ab201cc2b5eece9a18ee90dbf86cba6ce
shekhar-roy <shekharroy5292@gmail.com>
2024-03-18 14:36:24 +0530

    add collected images and responsive navbar styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "../src/components/Navbar.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-    <Navbar />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="/menu" element="" />
+          <Route path="/about" element="" />
+          <Route path="/ontact" element="" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 const Navbar = () => {
   return (
-    <div>
-        <div className='leftSide'>left side</div>
-        <div className='rightSide'>right side</div>
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={logo} alt="logo" />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button>
+        <ReorderIcon />
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
